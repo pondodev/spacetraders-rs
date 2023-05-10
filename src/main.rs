eff28f50d3161456688a8703c4a5b4d7b22e3cbf
Daniel Coady <me@pondo.dev>
2023-05-11 07:12:17 +1000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ use tokio::task;
 async fn main() {
     let api = API::new(true);
     let task_handle = task::spawn(async move {
-        async move {
-            match api.list_factions().await {
-                Ok(f)  => println!("got factions: {:?}", f),
-                Err(e) => println!("{}", e),
-            }
+        match api.list_factions().await {
+            Ok(f)  => println!("got factions: {:?}", f),
+            Err(e) => println!("{}", e),
+        }
+
+        match api.get_faction("foo").await {
+            Ok(f)  => println!("got faction: {:?}", f),
+            Err(e) => println!("{}", e),
         }
     });
 
-    tokio::join!(task_handle).0.unwrap().await;
+    tokio::join!(task_handle).0.unwrap();
 }
 
